25f31a0171f7adf7aa2bba2bee6e260496e1f76a
Leonardo Cassauara <124696947+leonardocassauara@users.noreply.github.com>
2023-05-15 10:56:41 -0300

    While loop, printf, basic syntax, int

---
+++ 9. Imprimir os valores de 1 até 10.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// Imprimir os números de 1 até 10 (while loop)
+
+int main(){
+	int contador=0;
+	
+	while (contador <= 9)
+	{
+		contador++;
+		printf("%d\n", contador);
+	}
+	return 0;
+}
