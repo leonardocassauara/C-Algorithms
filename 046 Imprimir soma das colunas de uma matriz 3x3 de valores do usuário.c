88df681cac08e750dca1931c61e4fa3f514037a1
Leonardo Cassauara <124696947+leonardocassauara@users.noreply.github.com>
2023-06-26 18:50:34 -0300

    Update

---
+++ 046 Imprimir soma das colunas de uma matriz 3x3 de valores do usuário.c
@@ -0,0 +1,48 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
+
+/* Problema: Imprimir a soma das linhas de uma matriz 3x3 de valores do usuário
+   Abstração:	1. Ler os valores da matriz usando a lógica do loop for
+   				2. Imprimir a matriz usando a lógica do loop for
+   				3. Fazer a soma das colunas da matriz somando todos os valores da coluna, imprimindo-o e resetando o seu valor logo depois
+*/
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+	int m[3][3], i=0, j=0, soma=0;
+    
+    for (i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("Digite um valor para a posiçao (%d, %d): \n", i, j);
+            scanf(" %d", &m[i][j]);
+        }
+    }
+    
+    printf("Matriz:\n");
+    
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            printf("[%d]\t", m[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("Soma das colunas:\n");
+    
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            soma = soma + m[j][i];
+        }
+        printf("[%d]\t", soma);
+        soma = 0;
+    }
+    
+    return 0;
+}
